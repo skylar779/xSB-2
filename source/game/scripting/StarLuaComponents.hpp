2f8a0cf9ff39c2a5bb7274f1aacde27081f1f5b1
skylar779 <89084411+skylar779@users.noreply.github.com>
2023-11-09 11:17:36 +0100

    Adjusted `script.updateDt` to be more accurate 1 / 60 should be more accurate than 0.01666666667

--- source/game/scripting/StarLuaComponents.hpp
+++ source/game/scripting/StarLuaComponents.hpp
@@ -272,7 +272,7 @@ float LuaUpdatableComponent<Base>::updateDt() const {
   float retDt = m_updatePeriodic.stepCount() * m_lastDt;
   // FezzedOne: Fix for a bug where this callback returns `0.0f` when called before the first `update`.
   if (retDt == 0.0f) {
-    return 0.01666666667f * GlobalTimescale;
+    return 1 * GlobalTimescale / 60;
   } else {
     return retDt;
   }
